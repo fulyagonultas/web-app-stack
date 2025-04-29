dc2517be02c42e8e227494af2183e92ea7fec333
fulyagonultas <fulyagonultas1995@gmail.com>
2025-04-29 16:52:35 +0200

    Cloud NAT created

--- k8s/nginx_ingress.tf
+++ k8s/nginx_ingress.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = "ingress-nginx"
-
+  name             = "nginx-ingress"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  timeout          = 600
   create_namespace = true
   version          = "4.7.0"
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,15 +12,31 @@ resource "google_compute_subnetwork" "subnet" {
   private_ip_google_access = true
 }
 
-resource "google_compute_firewall" "allow_http" {
-  name    = "allow-http"
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
   network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = ["443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
+}
+
+resource "google_compute_router" "nat_router" {
+  name    = "nat-router"
+  network = google_compute_network.vpc_network.id
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "cloud_nat" {
+  name                               = "cloud-nat"
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  enable_endpoint_independent_mapping = true
 }
