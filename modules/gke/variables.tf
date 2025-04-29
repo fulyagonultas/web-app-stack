bb33f10f8686c6575b5433c064f4f13c931a94db
fulyagonultas <fulyagonultas1995@gmail.com>
2025-04-29 18:03:36 +0200

    tf fmt fix

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -9,7 +9,7 @@ variable "subnet" {
 }
 variable "machine_type" {
   description = "The Nodepool Machine Type"
-  default = "e2-medium"
+  default     = "e2-medium"
 }
 variable "project_id" {
   description = "The GCP Project ID"
